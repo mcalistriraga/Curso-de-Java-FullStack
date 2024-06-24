b36d65aa70ec66fa066b5b3a15b8ff29cab1d6ee
manuelcalistri <mcalistri.freel@gmail.com>
2024-06-24 15:47:36 -0400

    actualizando el backend, pom.xls, application.properties, capas UsuarioDao, UsuarioDaoImp, Usuario.java, UsuarioControllers

--- src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
+++ src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
@@ -1,21 +1,21 @@
 package com.cursojava.curso.dao; // Define el paquete al que pertenece esta clase
 
-import com.cursojava.curso.models.Usuario; // Importa la clase Usuario desde el paquete de modelos
-import org.springframework.stereotype.Repository; // Importa la anotación Repository para indicar que esta clase es un repositorio
-import org.springframework.transaction.annotation.Transactional;  // Importa la anotación Transactional para la gestión de transacciones
+import com.cursojava.curso.models.Usuario;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager; // Importa la clase EntityManager para la gestión de entidades
-import javax.persistence.PersistenceContext; // Importa la anotación PersistenceContext para inyectar el EntityManager
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
-import java.util.List; // Importa la interfaz List para el manejo de listas
-@Repository // Indica que esta clase es un repositorio y se encargará de interactuar con la base de datos
-@Transactional // Indica que los métodos de esta clase deben ser gestionados dentro de una transacción
+import java.util.List;
+@Repository
+@Transactional
 public class UsuarioDaoImp implements UsuarioDao { // Implementa la interfaz UsuarioDao
-    @PersistenceContext // Inyecta una instancia de EntityManager
+    @PersistenceContext
     private EntityManager entityManager;  // Declara el EntityManager que se utilizará para interactuar con la base de datos
     @Override
-    public List<Usuario> getUsuarios() { // consulta a la BBDD, Implementa el método getUsuarios definido en la interfaz UsuarioDao
+    public List<Usuario> getUsuarios() { // consulta a la BBDD
         String query = "FROM Usuario";  // Define una consulta JPQL para obtener todos los objetos Usuario
-        return entityManager.createQuery(query).getResultList(); // Ejecuta la consulta y retorna el resultado
+        return entityManager.createQuery(query, Usuario.class).getResultList();
     }
 }
